a4f7aabba6d77ff8061652e14ddfe74bff6ab151
Angelo <angelo.antu@kireap.com>
2025-01-05 01:35:10 +0530

    changed color theme

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 
 function Home(){
     return(
-        <div className="bg-[#1C1917] flex flex-col space-y-4 items-center justify-center h-screen">
+        <div className="bg-[#FFFDD0] flex flex-col space-y-4 items-center justify-center h-screen">
 
-            <h1 className="text-5xl text-white font-extrabold ">Choose an Option    </h1>
+            <h1 className="text-5xl text-black font-extrabold ">Choose an Option    </h1>
             <button className="bg-[#2563EB] w-40 p-1 shadow-2xl rounded-lg text-2xl font-bold">
                 <Link to='/insights'>
                     Insights
